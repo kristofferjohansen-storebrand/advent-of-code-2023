a66ee2d0507dca7082495c98ba908457919f7051
Kristoffer Johansen <kristoffer.johansen@storebrand.no>
2023-12-03 20:11:12 +0100

    Day 3: Solve part one

---
+++ src/test/java/no/kristofferjohansen/adventofcode2023/day3/SolverTest.java
@@ -0,0 +1,37 @@
+package no.kristofferjohansen.adventofcode2023.day3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolverTest {
+
+    @Test
+    void solvePartOne() {
+        final String data = """
+                467..114..
+                ...*......
+                ..35..633.
+                ......#...
+                617*......
+                .....+.58.
+                ..592.....
+                ......755.
+                ...$.*....
+                .664.598..""";
+
+        assertEquals(4361, Solver.solvePartOne(data.lines().toList()));
+    }
+
+    @Test
+    void solvePartOneNoneAreAdjacent() {
+        final String data = """
+                .****.....
+                *....*....
+                *.12.*....
+                *....*....
+                .****.....""";
+
+        assertEquals(0, Solver.solvePartOne(data.lines().toList()));
+    }
+}
