b39b653cf7bfca3ec4c035261f344bacf705dddf
Kristoffer Johansen <kristoffer.johansen@storebrand.no>
2023-12-05 22:14:45 +0100

    Day 5: Solve part one

--- src/main/java/no/kristofferjohansen/adventofcode2023/day5/Solver.java
+++ src/main/java/no/kristofferjohansen/adventofcode2023/day5/Solver.java
@@ -3,12 +3,9 @@ package no.kristofferjohansen.adventofcode2023.day5;
 import no.kristofferjohansen.adventofcode2023.common.FileUtil;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
@@ -18,6 +15,8 @@ public class Solver {
     private enum MAP_TYPE {
         SOIL, FERTILIZER, WATER, LIGHT, TEMPERATURE, HUMIDITY, LOCATION
     }
+
+    private static CategoryEntry DEFAULT_CATEGORY_ENTRY = new CategoryEntry(0, 0, 0);
 
     public Solver() {
         try {
@@ -34,7 +33,7 @@ public class Solver {
     }
 
     static Long solvePartOne(final List<String> data) {
-        final List<Map<Long, Long>> categoryMapList = parseData(data);
+        final List<List<CategoryEntry>> categoryMapList = parseData(data);
         return Stream.of(data.get(0).substring(7).split(" "))
                 .map(String::trim)
                 .filter(value -> !value.isBlank())
@@ -43,7 +42,17 @@ public class Solver {
                 .min().orElse(-1);
     }
 
-    private static List<Map<Long, Long>> parseData(final List<String> data) {
+//    private static List<Map<Long, Long>> parseData(final List<String> data) {
+//        final int[] categoryDividerPositions = IntStream.range(0, data.size())
+//                .filter(iter -> data.get(iter).isBlank())
+//                .toArray();
+//
+//        return IntStream.range(0, categoryDividerPositions.length)
+//                .mapToObj(categoryIndex -> parseCategory(data, categoryDividerPositions, categoryIndex))
+//                .toList();
+//    }
+
+    private static List<List<CategoryEntry>> parseData(final List<String> data) {
         final int[] categoryDividerPositions = IntStream.range(0, data.size())
                 .filter(iter -> data.get(iter).isBlank())
                 .toArray();
@@ -53,20 +62,33 @@ public class Solver {
                 .toList();
     }
 
-    private static Map<Long, Long> parseCategory(final List<String> data, final int[] categoryDividerPositions, final int categoryIndex) {
+    private static List<CategoryEntry> parseCategory(final List<String> data, final int[] categoryDividerPositions, final int categoryIndex) {
         final int endOfCategoryIndex = categoryIndex == categoryDividerPositions.length - 1 ? data.size() : categoryDividerPositions[categoryIndex + 1];
         final List<String> categoryDataList = data.subList(categoryDividerPositions[categoryIndex]+2, endOfCategoryIndex);
 
-        final Map<Long, Long> categoryDataMap = new HashMap<>();
-        categoryDataList.stream()
+        return categoryDataList.stream()
                 .map(categoryDataLine -> Stream.of(categoryDataLine.split(" +"))
                         .map(String::trim)
                         .filter(value -> !value.isEmpty())
                         .mapToLong(Long::parseLong).toArray())
-                .forEach(categoryMapping -> addCategoryDataToMap(categoryDataMap, categoryMapping));
-
-        return categoryDataMap;
+                .map(categoryMapping -> new CategoryEntry(categoryMapping[1], categoryMapping[0], categoryMapping[2]))
+                .toList();
     }
+
+//    private static Map<Long, Long> parseCategory(final List<String> data, final int[] categoryDividerPositions, final int categoryIndex) {
+//        final int endOfCategoryIndex = categoryIndex == categoryDividerPositions.length - 1 ? data.size() : categoryDividerPositions[categoryIndex + 1];
+//        final List<String> categoryDataList = data.subList(categoryDividerPositions[categoryIndex]+2, endOfCategoryIndex);
+//
+//        final Map<Long, Long> categoryDataMap = new HashMap<>();
+//        categoryDataList.stream()
+//                .map(categoryDataLine -> Stream.of(categoryDataLine.split(" +"))
+//                        .map(String::trim)
+//                        .filter(value -> !value.isEmpty())
+//                        .mapToLong(Long::parseLong).toArray())
+//                .forEach(categoryMapping -> addCategoryDataToMap(categoryDataMap, categoryMapping));
+//
+//        return categoryDataMap;
+//    }
 
     private static Long processValue(final Long value, final ListIterator<Map<Long, Long>> categoryDataMapIterator) {
         if (categoryDataMapIterator.hasNext()) {
@@ -74,6 +96,23 @@ public class Solver {
         }
 
         return value;
+    }
+
+    private static long processValue(final long value, final ListIterator<List<CategoryEntry>> categoryDataListIterator) {
+        final List<CategoryEntry> categoryEntries = categoryDataListIterator.next();
+        final CategoryEntry closestCategoryEntry = findMatchingCategoryEntry(value, categoryEntries);
+        final long convertedValue = closestCategoryEntry.convertValue(value);
+        if (categoryDataListIterator.hasNext()) {
+            return processValue(convertedValue, categoryDataListIterator);
+        }
+
+        return convertedValue;
+    }
+
+    private static CategoryEntry findMatchingCategoryEntry(final long value, final List<CategoryEntry> categoryEntries) {
+        return categoryEntries.stream()
+                .filter(categoryEntry -> value >= categoryEntry.getSourceRange() && value < categoryEntry.getSourceRange() + categoryEntry.getMappingLength())
+                .findFirst().orElse(DEFAULT_CATEGORY_ENTRY);
     }
 
     private static void addCategoryDataToMap(final Map<Long, Long> categoryDataMap, final long[] categoryMapping) {
@@ -84,5 +123,29 @@ public class Solver {
 
     public static void main(String[] args) {
         new Solver();
+    }
+}
+
+class CategoryEntry {
+    private final long sourceRange;
+    private final long destinationRange;
+    private final long mappingLength;
+
+    public CategoryEntry(final long sourceRange, final long destinationRange, final long mappingLength) {
+        this.sourceRange = sourceRange;
+        this.destinationRange = destinationRange;
+        this.mappingLength = mappingLength;
+    }
+
+    public long getSourceRange() {
+        return sourceRange;
+    }
+
+    public long getMappingLength() {
+        return mappingLength;
+    }
+
+    public long convertValue(final long input) {
+        return mappingLength == 0 ? input : destinationRange + (input - sourceRange);
     }
 }
